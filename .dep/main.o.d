ba3573d98767f35603b21ff239b152a36383d123
ra9odd <seafox76@gmail.com>
2026-08-20 11:50:47 +0700

    Create main.o.d

---
+++ .dep/main.o.d
@@ -0,0 +1,83 @@
+main.o: main.c inc/K1921VG015.h \
+ c:\riscv-gcc\lib\gcc\riscv64-unknown-elf\12.2.1\include\stdint.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\stdint.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\machine\_default_types.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\features.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\_newlib_version.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\_intsup.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\_stdint.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\stdio.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\_ansi.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\newlib.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\config.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\machine\ieeefp.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\cdefs.h \
+ c:\riscv-gcc\lib\gcc\riscv64-unknown-elf\12.2.1\include\stddef.h \
+ c:\riscv-gcc\lib\gcc\riscv64-unknown-elf\12.2.1\include\stdarg.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\reent.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\_ansi.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\_types.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\machine\_types.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\lock.h \
+ c:\riscv-gcc\riscv64-unknown-elf\include\sys\stdio.h \
+ inc/system_k1921vg015.h inc/csr.h inc/stringify.h inc/arch.h inc/plic.h \
+ plib015/inc/plib015_adcsd.h plib015/inc/plib015.h \
+ plib015/inc/plib015_assert.h plib015/inc/plib015_version.h \
+ plib015/inc/plib015_conf.h plib015/inc/plib015_adcsar.h \
+ plib015/inc/plib015_adcsd.h plib015/inc/plib015_cmp.h \
+ plib015/inc/plib015_crc.h plib015/inc/plib015_hash.h \
+ plib015/inc/plib015_trng.h plib015/inc/plib015_crypto.h \
+ plib015/inc/plib015_dma.h plib015/inc/plib015_gpio.h \
+ plib015/inc/plib015_i2c.h plib015/inc/plib015_rcu.h \
+ plib015/inc/plib015_spi.h plib015/inc/plib015_tmr.h \
+ plib015/inc/plib015_tmr32.h plib015/inc/plib015_uart.h \
+ plib015/inc/plib015_wdt.h plib015/inc/plib015_qspi.h
+inc/K1921VG015.h:
+c:\riscv-gcc\lib\gcc\riscv64-unknown-elf\12.2.1\include\stdint.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\stdint.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\machine\_default_types.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\features.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\_newlib_version.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\_intsup.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\_stdint.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\stdio.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\_ansi.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\newlib.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\config.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\machine\ieeefp.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\cdefs.h:
+c:\riscv-gcc\lib\gcc\riscv64-unknown-elf\12.2.1\include\stddef.h:
+c:\riscv-gcc\lib\gcc\riscv64-unknown-elf\12.2.1\include\stdarg.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\reent.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\_ansi.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\_types.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\machine\_types.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\lock.h:
+c:\riscv-gcc\riscv64-unknown-elf\include\sys\stdio.h:
+inc/system_k1921vg015.h:
+inc/csr.h:
+inc/stringify.h:
+inc/arch.h:
+inc/plic.h:
+plib015/inc/plib015_adcsd.h:
+plib015/inc/plib015.h:
+plib015/inc/plib015_assert.h:
+plib015/inc/plib015_version.h:
+plib015/inc/plib015_conf.h:
+plib015/inc/plib015_adcsar.h:
+plib015/inc/plib015_adcsd.h:
+plib015/inc/plib015_cmp.h:
+plib015/inc/plib015_crc.h:
+plib015/inc/plib015_hash.h:
+plib015/inc/plib015_trng.h:
+plib015/inc/plib015_crypto.h:
+plib015/inc/plib015_dma.h:
+plib015/inc/plib015_gpio.h:
+plib015/inc/plib015_i2c.h:
+plib015/inc/plib015_rcu.h:
+plib015/inc/plib015_spi.h:
+plib015/inc/plib015_tmr.h:
+plib015/inc/plib015_tmr32.h:
+plib015/inc/plib015_uart.h:
+plib015/inc/plib015_wdt.h:
+plib015/inc/plib015_qspi.h:
